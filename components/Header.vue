b727e781a03df8abd3fd0a264de18a9a8e08c090
Hombre-x <gabisanpar@gmail.com>
2024-01-03 16:05:31 -0500

    💄Added <Header/> component
    It has the Logo and the NavBar components, as well as the UserInfo information

---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <UserInfo id="user-info-component"/>
+    <Logo id="logo-component"/>
+    <NavBar id="navbar-component"/>
+  </header>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="sass">
+
+  header
+    display: flex
+    flex-direction: row
+    justify-content: space-between
+    align-items: center
+
+  #user-info-component, #navbar-component
+    flex: 1
+
+  #logo-component
+    flex: 2
+
+</style>
