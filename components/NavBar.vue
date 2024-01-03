df0cd63992b9a50082cfb424732278634fa6cc1d
Hombre-x <gabisanpar@gmail.com>
2024-01-03 16:03:12 -0500

    💄Added <NavBar/> component
    The NavBar component has the two login buttons on it. Maybe it will contain more in the future

---
+++ components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+    <ul>
+      <li><ButtonLogin id="btn-signup">Sign Up</ButtonLogin></li>
+      <li><ButtonLogin id="btn-login">Log In</ButtonLogin></li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="sass">
+
+  @import '/assets/styles/colours'
+
+  nav
+
+    ul
+      display: flex
+      justify-content: space-around
+      align-items: center
+      list-style: none
+      padding: 0
+      margin: 0
+      
+
+  #btn-signup
+    background-color: $secondary-colour
+    border: 5px solid $secondary-colour
+    color: $text-colour
+
+  #btn-login
+    border: 5px solid $primary-colour
+    color: $primary-colour
+
+</style>
