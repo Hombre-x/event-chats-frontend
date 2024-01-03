2c69347a0e251f95b3b5cc391c7ed802027365e8
Hombre-x <gabisanpar@gmail.com>
2024-01-03 15:57:25 -0500

    🔧 Updated nuxt.config.ts
    Added webapp properties like a favicon, tittle and charset.
    
    Also, added the Google Fonts plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-8',
+      title: "VentChats",
+      link: [{ rel: 'icon', type: 'image/svg-xml', href: '/favicon.svg'}]
+    }
+  },
+  modules: ['@nuxtjs/google-fonts', "@nuxt/image"],
+
+  // Fonts
+  googleFonts: {
+    families: {
+      "Rethink Sans" : true
+    }
+  }
 })
