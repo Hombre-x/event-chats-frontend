5dd5fe691aa192dc4022c407d0c6063c92392f21
Hombre-x <gabisanpar@gmail.com>
2024-01-03 16:01:53 -0500

    💄Added <ButtonLogin> component
    It is the component for the Login and Signup buttons on the NavBar

---
+++ components/ButtonLogin.vue
@@ -0,0 +1,36 @@
+<template>
+  <button @click="showAlert"><slot></slot></button>   
+</template>
+
+<script lang="ts" setup>
+
+const showAlert = () => {
+  alert("You clicked the button!")
+}
+
+</script>
+
+<style lang="sass">
+
+  @import '/assets/styles/colours'
+  @import '/assets/styles/typescale'
+
+  button
+    all: unset
+    cursor: pointer
+
+    width: 7vw
+    height: 100%
+
+    border-radius: 50px
+    padding: 5px
+
+
+    text-align: center
+
+    font-size: $perfect-fourth * 1rem
+    font-weight: 500
+
+    
+
+</style>
