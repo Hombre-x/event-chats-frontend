8e5afd4f3daaad40aad999653a536a3bff0892cb
Hombre-x <gabisanpar@gmail.com>
2024-03-05 23:23:22 -0500

    🗺️ Added state userStore.ts composable

---
+++ composables/userStore.ts
@@ -0,0 +1,14 @@
+import { defineStore } from "pinia"
+import type { Ref } from "vue"
+
+import { genAnonymousName } from "~/src/user/info/anonymous-name"
+
+const userStoreSetup = (): { name: Ref<string> } => {
+  const name = ref("Anonymoud Giraffe")
+
+  name.value = genAnonymousName()
+
+  return { name }
+}
+
+export const useUserStore = defineStore("userInfo", userStoreSetup)
