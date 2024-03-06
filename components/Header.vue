ba7328e7d2103989e020f1d78539238995cbe2c7
Hombre-x <gabisanpar@gmail.com>
2024-03-05 23:17:03 -0500

    💄Updated <Header> component
    It has padding now

--- components/Header.vue
+++ components/Header.vue
@@ -1,27 +1,26 @@
 <template>
   <header>
-    <UserInfo id="user-info-component"/>
-    <Logo id="logo-component"/>
-    <NavBar id="navbar-component"/>
+    <UserInfo id="user-info-component" />
+    <Logo id="logo-component" />
+    <NavBar id="navbar-component" />
   </header>
 </template>
 
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 
 <style lang="sass">
 
-  header
-    display: flex
-    flex-direction: row
-    justify-content: space-between
-    align-items: center
+header
+  display: flex
+  flex-direction: row
+  justify-content: space-between
+  align-items: center
+  padding: 25px
 
-  #user-info-component, #navbar-component
-    flex: 1
 
-  #logo-component
-    flex: 2
+#user-info-component, #navbar-component
+  flex: 1
 
+#logo-component
+  flex: 1
 </style>
