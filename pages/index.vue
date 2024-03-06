a42342e612a42beb92ff4c28c4c54eef353f56fb
Hombre-x <gabisanpar@gmail.com>
2024-03-05 23:31:04 -0500

    💄 Removed bottom text and added <Chat> and <OnlineUsers> components

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,22 @@
 <template>
-  <Header/>
-  <section>
-    <h3>Bottom text</h3>
+  <Header />
+  <section id="home-body">
+    <OnlineUsers />
+    <Chat class="chat" />
   </section>
 </template>
+
+<style lang="sass" scoped>
+
+#home-body
+  display: flex
+  flex-direction: column
+  justify-content: flex-end
+  align-items: center
+  gap: 30px
+
+  width: 100%
+  padding: 25px
+  height: 100%
+  flex-grow: 1
+</style>
