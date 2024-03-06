af7dfcd1f2e1a03e2a82c6a0b15c1bd1bf6ff041
Hombre-x <gabisanpar@gmail.com>
2024-03-05 23:17:37 -0500

    💄Changed directory of logo SVG

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,39 +1,39 @@
 <template>
   <div class="logo-container">
-    <NuxtImg src="/images/wind.svg" id="logo-img"/>
-    <h3 id="logo-text">
+    <NuxtImg src="../public/svgs/wind.svg" id="logo-img" />
+    <div id="logo-text">
       <span id="logo-text-vent">Vent</span>
       <span id="logo-text-chat">Chats</span>
-    </h3>
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 
 <style lang="sass">
 
-  @import '/assets/styles/colours'
+@import '/assets/styles/colours'
+@import '/assets/styles/typescale'
 
-  .logo-container
-    display: flex
-    flex-direction: row
-    justify-content: center
-    align-items: center
-
-    margin: 10px
-  
+.logo-container
+  display: flex
+  flex-direction: row
+  justify-content: center
+  align-items: center
 
 
 
-  #logo-img
-    width: auto
-    height: 60px
 
 
-  #logo-text
-    #logo-text-vent
-      color: $accent-colour
+#logo-img
+  width: auto
+  height: 60px
 
+
+#logo-text
+  font-size: perfect-fourth-typescale(4)
+  font-weight: bold
+
+  #logo-text-vent
+    color: $accent-colour
 </style>
