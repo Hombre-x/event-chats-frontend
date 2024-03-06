647e2e50622804c6c7c234f9d3760fa6e7e7d0f4
Hombre-x <gabisanpar@gmail.com>
2024-03-05 23:07:34 -0500

    💄 Added logic and style to the <ButtonBase> component

---
+++ components/ButtonBase.vue
@@ -0,0 +1,43 @@
+<template>
+  <button @click="emitClick"><slot></slot></button>
+</template>
+
+<script lang="ts" setup>
+const emits = defineEmits<{
+  (e: "click", payload: MouseEvent): void
+}>()
+
+const emitClick = (payload: MouseEvent): void => {
+  emits("click", payload)
+}
+</script>
+
+<style lang="sass" scoped>
+
+@use 'sass:math'
+@import '/assets/styles/colours'
+@import '/assets/styles/typescale'
+
+button
+
+  all: unset
+  display: flex
+  justify-content: center
+  align-items: center
+
+  $height: 30px
+
+  cursor: pointer
+
+  width: 120px
+  height: $height
+
+  border-radius: $height
+  padding: 10px
+
+
+  text-align: center
+
+  font-size: 20px
+  font-weight: bold
+</style>
