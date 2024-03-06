6a571069ac10b0de058129e98a0ed2c933bfd1a6
Hombre-x <gabisanpar@gmail.com>
2024-03-05 23:19:38 -0500

    💄 Added <OnlineUsers> component
    It will show the number of current online users in the future

---
+++ components/OnlineUsers.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <span class="online-users-text">Users Online: </span>
+    <span class="online-users-number">{{ numberOfOnlineUsers }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const numberOfOnlineUsers = ref(0)
+</script>
+
+<style lang="sass">
+
+@import '/assets/styles/colours'
+@import '/assets/styles/typescale'
+
+div
+  .online-users-text
+    font-size: perfect-fourth-typescale(3)
+    color: $text-colour
+    font-weight: bold
+
+  .online-users-number
+    font-size: perfect-fourth-typescale(3)
+    color: $accent-colour
+    font-weight: bold
+</style>
