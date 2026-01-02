26d8d73159a06be6338502774e0465226f075d35
Hombre-x <gabisanpar@gmail.com>
2026-01-01 20:26:22 -0500

    ♻️ Move to the new app folder

--- app/components/UserInfoName.vue
+++ app/components/UserInfoName.vue
@@ -3,8 +3,8 @@
 </template>
 
 <script lang="ts" setup>
-import { useUserStore } from "~/composables/userStore"
-import { perfectFourthTypescale } from "~/src/lib/fonts/typescale"
+import { useUserStore } from "@/composables/userStore"
+import { perfectFourthTypescale } from "@@/src/lib/fonts/typescale"
 
 const props = defineProps<{
   fontLevel: number
--- app/composables/userStore.ts
+++ app/composables/userStore.ts
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia"
 import type { Ref } from "vue"
 
-import { genAnonymousName } from "~/src/user/info/anonymous-name"
+import { genAnonymousName } from "@@/src/user/info/anonymous-name"
 
 const userStoreSetup = (): { name: Ref<string> } => {
   const name = ref("Anonymoud Giraffe")
