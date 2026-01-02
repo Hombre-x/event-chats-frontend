636ebdb813aed3e7c24604524f4827912cd7a111
Hombre-x <gabisanpar@gmail.com>
2026-01-01 20:04:03 -0500

    💄 Solved Boring Avatar issue Now it uses the native React component instead of the url API

--- components/BoringAvatar.vue
+++ components/BoringAvatar.vue
@@ -1,36 +1,37 @@
 <template>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    :width="width"
-    :height="height"
-    :viewBox="viewBox"
-    fill="none"
-  >
-    <circle
-      :cx="radius + 2"
-      :cy="radius + 2"
-      :r="radius"
-      fill="#545454"
-      stroke="#E5E8EB"
-      stroke-width="3"
-    />
+  <div class="avatar-container">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      :width="width"
+      :height="height"
+      :viewBox="viewBox"
+      fill="none"
+      class="avatar-outer-circle-svg"
+    >
+      <circle
+        :cx="radius + strokeWidth / 2"
+        :cy="radius + strokeWidth / 2"
+        :r="radius"
+        fill="#545454"
+        stroke="#E5E8EB"
+        :stroke-width="strokeWidth"
+      />
+    </svg>
 
     <Avatar 
-      :size="props.size"
+      class="boring-avatar"
+      :size="props.size - strokeWidth"
       :name="props.name"
       :square="props.square"
       :variant="props.variant"
       :colors="props.colors"
-      />
-      
-  </svg>
+    />
+  </div>
 </template>
 
-<script lang="tsx" setup>
-import { boringAvatarUrl } from "~/src/user/info/boring-avatar"
+<script lang="ts" setup>
 import { applyReactInVue } from "veaury"
 import type { DefineComponent } from 'vue'
-
 import AvatarReact from 'boring-avatars'
 
 interface AvatarProps {
@@ -50,22 +51,27 @@ const props = withDefaults(defineProps<AvatarProps>(), {
   colors: () => ["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"],
 })
 
-const width = computed(() => props.size + 4)
-const height = computed(() => props.size + 4)
+// Stroke width constant
+const strokeWidth = 4
+
+const width = computed(() => props.size + strokeWidth)
+const height = computed(() => props.size + strokeWidth)
 const viewBox = computed(() => `0 0 ${width.value} ${height.value}`)
 const radius = computed(() => props.size / 2)
 
-const avatarSvg = boringAvatarUrl(
-  props.size,
-  props.name,
-  false,
-  props.variant,
-  props.colors,
-)
+const cssWidth = computed(() => `${width.value}px`)
+const cssHeight = computed(() => `${height.value}px`)
+const cssStrokeWidth = computed(() => `${strokeWidth}px`)
 </script>
 
-<style lang="sass">
+<style lang="sass" scoped>
+.avatar-container
+  position: relative
+  width: v-bind(cssWidth)
+  height: v-bind(cssHeight)
 
-    
-
+.boring-avatar
+  position: absolute
+  top: v-bind(cssStrokeWidth)
+  left: v-bind(cssStrokeWidth)
 </style>
